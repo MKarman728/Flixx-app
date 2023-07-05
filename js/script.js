f84f306b41d1ac885c0b7f1d84ea3b009cd055ae
Mark Karman <mkarman08@gmail.com>
2023-07-05 16:41:20 -0700

    working on search capabilities

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 const global = {
-    currentPage: window.location.pathname
+    currentPage: window.location.pathname,
+    search: {
+      term:'',
+      type: '',
+      page: 1,
+      totalPages: 1
+    },
+    api: {apiKey: '583b933176ae70467201e064968cf907',
+        apiURL: 'https://api.themoviedb.org/3/'
+  }
 }
 
 //Displays popular movies
@@ -246,7 +255,25 @@ async function displaySliderTV(){
   })
 
 }
+//Search Movies/Shows
+async function search(){
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  global.search.type = urlParams.get('type');
+  global.search.term = urlParams.get('search-term');
 
+  if(global.search.term !== '' && global.search.term !== null){
+    //@todo - make request and display results
+    const results = await searchAPIData();
+    console.log(results);
+  }
+  else{
+    showAlert('Please enter a search term');
+  }
+}
+
+
+//Display slider movies
 function initSwiper(){
   const swiper = new Swiper('.swiper',{
     slidesPerView: 1,
@@ -273,13 +300,24 @@ function initSwiper(){
 
 //Fetch data from TMDB API
 async function fetchAPIData(endpoint){
-    const API_KEY = '583b933176ae70467201e064968cf907';
-    const API_URL = 'https://api.themoviedb.org/3/';
+    const API_KEY = global.api.apiKey;
+    const API_URL = global.api.apiURL;
     showSpinner();
     const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
     const data = await response.json();
     hideSpinner();
     return data;
+}
+
+//Make Request to Search
+async function searchAPIData(){
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiURL;
+  showSpinner();
+  const response = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`);
+  const data = await response.json();
+  hideSpinner();
+  return data;
 }
 
 //Highlight Active Link
@@ -290,6 +328,16 @@ function highlightActiveLink(){
             link.classList.add('active');
         }
     })
+}
+
+// Show Alert
+function showAlert(message, className){
+  const alertEl = document.createElement('div');
+  alertEl.classList.add('alert', className);
+  alertEl.appendChild(document.createTextNode(message));
+  document.querySelector('#alert').appendChild(alertEl);
+
+  setTimeout(()=> alertEl.remove(), 3000)
 }
 
 function addCommasToNumber(num){
@@ -316,7 +364,7 @@ function init(){
             displayShowDetails();
             break;
         case '/search.html':
-            console.log('Search');
+            search();
             break;
     }
     highlightActiveLink();
